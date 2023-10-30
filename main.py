bd23101db42a1709220a67ec9002ac76fe3ce821
GabrielaP05 <149349520+GabrielaP05@users.noreply.github.com>
2023-10-29 22:44:55 -0600

    Add files via upload
    proyecto1

---
+++ main.py
@@ -0,0 +1,47 @@
+# Tipos de variables primitivas
+string = "Proyecto" ,"Investigacion"
+int = 4
+float = 4.5
+boolean = True
+
+# Concatenación de las variables
+resultado_concatenacion = (string , (int) , (float) , (boolean))
+
+# Límites de enteros y flotantes en Python
+Investigacion="""
+En Python el tamaño de los enteros es flexible. En versiones de 32 bits, el rango es de -2^31 a 2^31 - 1.
+En versiones de 64 bits, el rango es mucho mayor, desde -2^63 a 2^63 - 1.
+Los flotantes siguen el estándar IEEE 754 de 64 bits, por lo que tienen una precisión de 15-16 dígitos decimales.
+Python también tiene constantes para límites, como sys.maxsize y sys.float_info.
+"""
+
+# Fórmula para la suma de los primeros n números pares
+# La fórmula es: n * (n + 1)
+n = 5  
+suma_pares = n * (n + 1)
+
+#  resultados
+print("Concatenación de variables:", resultado_concatenacion)
+print( "Límites de enteros y flotantes en Python:", Investigacion)
+print("Suma de los primeros números pares:", suma_pares)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
